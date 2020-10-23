d2f921a3f817d8ea8b225d701cf65ba37720fa3e
vacer1997 <70571233+vacer1997@users.noreply.github.com>
2020-10-23 13:52:49 +0200

    Create bmiapplicatie.php

---
+++ Website/bmiapplicatie.php
@@ -0,0 +1,29 @@
+<?php
+$title = "BMI Applicatie";
+?>
+
+<?php include "includes/header.php"; ?>
+<style>
+<?php include "css/style3.css"; ?>
+</style>
+
+<header><img src="img/logo.png" alt="HTML tutorial" style="width:75px;height:75px;"> BMI Applicatie </header>
+
+<body>
+
+<div class="wrapper">
+    <nav class="flex-nav">
+      <ul>
+        <li><a href="voorpagina.php">Home</a></li>
+        <li><a href="bmiform.php">BMI Rekenmachine</a></li>
+        <li><a href="recepten.php">Gezonde Recepten</a></li>
+        <li><a href="bmiapplicatie.php">BMI Applicatie</a></li>
+        <li><a href="quiz.php">Quiz</a></li>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+</body>
+
+<?php include "includes/footer.php"; ?>
