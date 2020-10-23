ebcd1b449864bbaada513fdc3d3489b9e3aa9636
vacer1997 <70571233+vacer1997@users.noreply.github.com>
2020-10-23 13:53:12 +0200

    Create recepten.php

---
+++ Website/recepten.php
@@ -0,0 +1,62 @@
+<?php
+$title = "Gezonde Recepten";
+?>
+
+<?php include "includes/header.php"; ?>
+<style>
+<?php include "css/style3.css"; ?>
+</style>
+
+<header><img src="img/logo.png" alt="HTML tutorial" style="width:75px;height:75px;"> Gezonde Recepten</header>
+
+<body>
+
+  <div class="wrapper">
+    <nav class="flex-nav">
+      <ul>
+        <li><a href="voorpagina.php">Home</a></li>
+        <li><a href="bmiform.php">BMI Rekenmachine</a></li>
+        <li><a href="recepten.php">Gezonde Recepten</a></li>
+        <li><a href="bmiapplicatie.php">BMI Applicatie</a></li>
+        <li><a href="quiz.php">Quiz</a></li>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <div class="recept1">
+    <div>
+      <li>400 gr bloemkoolrijst</li>
+      <li>1 gr klein blikje maïs a 140</li>
+      <li>1 mini blikje kidneybonen a 80 gr</li>
+      <li>1 paprika</li>
+      <li>1 bosui</li>
+      <li>3 tomaten</li>
+      <li>1 rode ui</li>
+      <li>2 tenen knoflook</li>
+      <li>2 eetlepels tacokruiden (of andere mexicaanse kruiden)</li>
+      <li>1 eetlepel limoensap</li>
+      <li>1 eetlepel olie</li>
+      <li>125 ml zure room (of sojayoghurt)</li>
+    </div>
+    <div> 
+      <b>Mexicaanse bloemkoolrijst</b><br><br>
+      Snipper de ui en knoflook fijn en fruit aan in een pan met een eetlepel olie. <br>
+      Snij de paprika in kleine blokjes en voeg toe.
+    </div>
+    <div>
+      Doe de bloemkoolrijst er bij en bak een paar minuutjes mee. Roer de kruiden er door.<br>
+      Laat de bonen en mais uitlekken en schep door de bloemkoolrijst. Snijd de tomaten in blokjes,<br>
+      verwijder het waterige gedeelte, en schep ook door het mengsel.
+    </div>
+    <div>
+    Voeg de limoensap toe en hak de bosui fijn. Schep de helft van de bosui door de Mexicaanse bloemkoolrijst<br>
+     en gebruik de rest om het gerecht te garneren. Serveer de bloemkoolijst met de zure room.
+    </div>
+    <div>
+    <img src="img/gerecht.jpg" alt="gerecht" style="width:300px;height:300px;">
+    </div>
+
+</body>
+
+</html>
