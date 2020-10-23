8cf4a4e46f2586f61eec67e6d5c7673ba532c682
vacer1997 <70571233+vacer1997@users.noreply.github.com>
2020-10-23 13:47:29 +0200

    Create voorpagina.php

---
+++ Website/voorpagina.php
@@ -0,0 +1,58 @@
+<?php
+$title = "Home";
+?>
+
+<?php include "includes/header.php"; ?>
+<style>
+<?php include "css/style3.css"; ?>
+</style>
+
+<header><img src="img/logo.png" alt="HTML tutorial" style="width:75px;height:75px;"> Home </header>
+
+<body>
+
+<div class="wrapper">
+    <nav class="flex-nav">
+      <ul>
+        <li><a href="voorpagina.php">Home</a></li>
+        <li><a href="bmiform.php">BMI Rekenmachine</a></li>
+        <li><a href="recepten.php">Gezonde Recepten</a></li>
+        <li><a href="bmiapplicatie.php">BMI Applicatie</a></li>
+        <li><a href="quiz.php">Quiz</a></li>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+</br>
+
+<div class="inlichting">
+
+    Welkom op onze website! Deze website is aangemaakt om jongeren te stimuleren om gezonder te leven.</br>
+    Hier kan je lezen over waarom gezond eten zo belangrijk is, en wat de voordelen zijn van gezond eten.</br>
+    Daarnaast hebben we nog lekkere gezonde recepten en is het ook mogelijk om je BMI te berekenen!</br>
+
+    <h2>Waarom is gezond eten belangrijk?</h2>
+
+    Behalve dat gezond eten heel erg lekker is, is het ook belangrijk. </br>
+    De afgelopen jaren zijn we ons met zijn alle veel meer bewust geworden van wat wij allemaal naar binnen werken en is gezonde voeding een ware trend geworden. </br>
+    Maar waarom is gezond eten dan zo belangrijk? En wat moet je doen om gezond te eten? Daarover vertel ik je graag meer in dit blog bericht.</br>
+
+    <h2>Een goed gevoel door te eten!</h2>
+    Gezond eten, en ook ongezond eten, heeft een grote invloed op hoe je je voelt. Steeds meer zijn wij bezig met bewuste en gezonde voeding.</br>
+    Zo willen we weten wat we eten en waar het eten vandaan komt, en natuurlijk welke voedingsstoffen het bevat.</br>
+    Via televisie, sociale media en andere kanalen word je bedolven met gezonde recepten. De een nog gezonder en mooier dan de ander.</br>
+    Maar vergeet niet waarom een gezonde levensstijl zo belangrijk is, want gezond eten is veel leuker en makkelijker als je weet waarom je het doet.</br>
+
+    <h2>Het doel van gezond eten</h2>
+    Gezond eten is per definitie geen doel. Eigenlijk is het een middel. Een middel om te zorgen dat jij je goed voelt.</br>
+    Gezonde voeding heeft op diverse manieren effect op ons lichaam, zowel van binnen als van buiten.</br>
+    Hieronder vind je zeven redenen waarom gezond eten zo belangrijk is voor jou.</br>
+
+</div>
+
+
+    
+</body>
+
+<?php include "includes/footer.php"; ?>
